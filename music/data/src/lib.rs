dc80da9c8274a3f67b450745a5bdf4c8a1fb9272
pixeledadler <vilis@taksis.org>
2020-12-10 23:08:32 +0200

    Changes

--- music/data/src/lib.rs
+++ music/data/src/lib.rs
@@ -1,9 +1,51 @@
-
-
 #[cfg(test)]
 mod tests {
     #[test]
     fn it_works() {
         assert_eq!(2 + 2, 4);
+    }
+}
+
+
+// This class implements a methid to communicate woth a SQLite DB for music 
+// track specifics. 
+// Checks, if database exist
+// If it doesn't makes a new database with two tables
+// 1. for music track information
+// - track ID
+// - title
+// - artist
+// - file location
+// 2. playlists
+// - playlist id
+// - track id
+
+mod database {
+    struct Tables{
+        name: String,
+    }
+    struct MusicTrack {
+        id: i32,
+        title: String,
+        artist: String,
+        file_path: String
+    }
+    
+    pub fn database_connection () -> bool {
+        let connection = rusqlite::Connection::open("music_db.sql");
+        let table_table = connection.execute("SELECT name FROM sqlite_master;");
+        for table in table_table {
+            if table.name == "tracks"{
+                return true;
+
+            }
+        }
+        connection.execute("
+        CREATE TABLE tracks(
+            id INTEGER PRIMARY KEY,
+            title TEXT,
+            artist TEXT,
+            file_path TEXT)", rusqlite::NO_PARAMS);
+        return false;
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,4 @@
-fn main() -> std::io::Result<()> {
-    use std::fs;
-    
-    let music_dir = fs:::wq
-
-    let metadata = fs::metadata("/home/ubuntu/music/Music/Laffey - Summer Nights.mp3")?;
-
-    println!("{:?}", metadata.file_type());
-    Ok(())
+fn main() {
+    println!("Hi!");
 }
 
